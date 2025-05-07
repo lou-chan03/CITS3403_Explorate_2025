ef0ff34cf97e56e6d6ebf5add4387300ff4ca45e
lou-chan03 <louisechan03@gmail.com>
2025-05-07 21:32:21 +0800

    Added Star Rating JS to rate-page and script to script.js, made base.html to figure out flask, altered some of the share page css

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -117,3 +117,32 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
   
+// Star Rating Script
+
+  // access the stars
+  let stars = document.getElementsByClassName("star");
+  let output = document.getElementById("output");
+
+  // Function to update the rating
+  function gfg(n) {
+    let cls = "";
+    remove();
+
+    if (n == 1) cls = "one";
+    else if (n == 2) cls = "two";
+    else if (n == 3) cls = "three";
+    else if (n == 4) cls = "four";
+    else if (n == 5) cls = "five";
+      
+    for (let i = 0; i < n; i++) {
+      stars[i].className = "star" + cls;
+    }
+    output.innerText = "Rating is:  " + n + "/5";
+  }
+
+  // Function to remove pre-applied styling
+  function remove() {
+    for (let i = 0; i < 5; i++) {
+        stars[i].className = "star";
+    }
+  }
