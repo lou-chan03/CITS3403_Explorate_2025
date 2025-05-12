9a0c0ad2c376efbd2124165c6a1f652f7b059ef3
grumblingremlin <ishitasharma4103@gmail.com>
2025-05-12 19:22:22 +0800

    I have added the user database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
@@ -37,4 +38,18 @@ class UserSelection(db.Model):
 
     def __repr__(self):
         return f'<UserSelection {self.id} - Session: {self.session_id}>'
+
+
+# User model (DB schema)
+
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
