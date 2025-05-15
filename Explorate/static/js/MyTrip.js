7ae1262161aede8f98de43b7b372f2f9b8928d48
Luvish Chundra Ramchurn <luvishramchurn1@gmail.com>
2025-05-16 06:55:48 +0800

    Add MyTrip page,fixed the rating page linking

---
+++ Explorate/static/js/MyTrip.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $.ajax({
+        url: '/api/adventures',  // relative URL is better
+        type: 'POST',
+        contentType: 'application/json',
+        data: '{}',  // send empty JSON, or you can omit this line if your Flask can handle no data
+        success: function (data) {
+            if (data.length > 0) {
+                const table = $('#adventure-table');
+                const tbody = table.find('tbody');
+                tbody.empty();
+
+                data.forEach(row => {
+                    const tr = `<tr>
+                        <td>${row.adventure_name}</td>
+                        <td>${row.recommendation_1}</td>
+                        <td>${row.recommendation_2}</td>
+                        <td>${row.recommendation_3}</td>
+                        <td>${row.recommendation_4}</td>
+                    </tr>`;
+                    tbody.append(tr);
+                });
+
+                table.show();
+                $('#no-data-message').hide();
+            } else {
+                $('#adventure-table').hide();
+                $('#no-data-message').show();
+            }
+        },
+        error: function () {
+            alert('Failed to fetch data.');
+        }
+    });
+});
+
